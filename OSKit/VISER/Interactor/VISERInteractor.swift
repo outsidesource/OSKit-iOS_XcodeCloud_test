889c7f7dda37ffce8b77b1928baf84b142409700
Brody Robertson <bdrobert@outsidesource.com>
2021-09-14 11:04:56 -0400

    Extracted RxVISERInteractor

--- OSKit/VISER/Interactor/VISERInteractor.swift
+++ OSKit/VISER/Interactor/VISERInteractor.swift
@@ -7,17 +7,13 @@
 //
 
 import Resolver
-import RxSwift
 
-// FIXME: - RENAME RxVISERInteractor
 open class VISERInteractor<S: IState>: IInteractor {
     
     public let id: String
-    public let disposeBag = DisposeBag()
     
     @Injected public private(set) var actionDispatcher: IActionDispatcher
     public lazy var statePresenter: StatePresenter<S> = Resolver.root.resolve(args: self.id)
-    public lazy var rxStatePresenter: RxStatePresenter<S> = Resolver.root.resolve(args: self.id)
     
     deinit {
         self.dispatch(StateContainerAction.deinitState(id))
@@ -26,6 +22,7 @@ open class VISERInteractor<S: IState>: IInteractor {
     public init(_ id: String = UUID().uuidString) {
         self.id = id
         
+        // TODO: - 0 IMPROVE by injecting initial state and dispatch StateContainerAction.initState here
         /// It is the responsibility of subclasses to inflate initial state and dispatch accordingly
         
     }
